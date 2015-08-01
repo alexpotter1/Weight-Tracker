7934ae24da6aab6940a19b1d6b7dc82f36134e30
Alex Potter <alexanderpotter2010@gmail.com>
2015-08-01 12:03:00 +0100

    Initialise project

---
+++ Project/Weight Tracker/Weight Tracker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Weight Tracker
+//
+//  Created by Alex Potter on 27/08/2015.
+//  Copyright © 2015 Alex Potter. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
