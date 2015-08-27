cd102ce766d666c8a3a05f4e58c438553ce85c0b
Alex Potter <alexanderpotter2010@gmail.com>
2015-08-27 22:10:06 +0100

    Added main window and fixed bugs
    Bugs relating to the list of names in the NSComboBox are fixed

--- Project/Weight Tracker/Weight Tracker/AppDelegate.swift
+++ Project/Weight Tracker/Weight Tracker/AppDelegate.swift
@@ -17,10 +17,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var ContinueButton: NSButton!
     @IBOutlet weak var UserComboBox: NSComboBox!
     
-    /* Keeps a reference to the window controller that we're gonna switch to when
-    we click the New User button, so that it doesn't get deallocated (otherwise, errors) */
+    /* Keeps a reference to the window controllers that we're gonna switch to, so that it doesn't get deallocated (otherwise, errors) */
     // In Cocoa, windows/views are loaded lazily instead of all at once (better performance)
     let newUserVC = NewUserWindowController(windowNibName: "NewUserWindow")
+    let mainWindowVC = MainWindowController(windowNibName: "MainWindow")
 
     @IBAction func NewUserButtonClicked(sender: NSButton) {
         /* Begins the modal sheet animation when the New User button is clicked;
@@ -30,21 +30,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @IBAction func ContinueButtonClicked(sender: NSButton) {
+        print("loading MainWindowController")
+        // Loads the main window from the xib file (so that the user interface for that controller is loaded)
+        mainWindowVC.loadWindow()
+        mainWindowVC.windowDidLoad()
+        mainWindowVC.showWindow(mainWindowVC.window) // The argument to this function is the target window we want to show
     }
     
+    func populateNSComboBoxWhenAppLaunches() {
+        print("Populating combo box initially")
+        
+        /* The reason why this line exists more than once is that 
+        we want it to reload the NSUserDefaults object to get new names */
+        let names = NSUserDefaults.standardUserDefaults().objectForKey("NewUserNames")
+        
+        // Checking if the object that stores the names is empty in NSUserDefaults
+        if names != nil {
+            // Adding all elements in array because we initially want every name on startup
+            
+            /* as! forces downcast to array of Strings from array of AnyObject, for type safety
+            The app will crash though if this doesn't exist, but this function should only be called
+            if this array exists in NSUserDefaults */
+            let nameArray = names as! [String]
+            
+            // Loops through all the names in the array to make sure all are added to the NSComboBox
+            for name in nameArray {
+                UserComboBox.addItemWithObjectValue(name)
+            }
+        }
+    }
     
     // Function that will fill the NSComboBox with users from persistent storage (NSUserDefaults)
     // This will run when a NSNotification is received from the function that saves the data
     func populateNSComboBox(notification: NSNotification) {
         print("Populating combo box")
         
+        /* The reason why this line exists more than once is that
+        we want it to reload the NSUserDefaults object to get new names */
+        let names = NSUserDefaults.standardUserDefaults().objectForKey("NewUserNames")
+        
         /* as! forces downcast to array of Strings from array of AnyObject, for type safety
         The app will crash though if this doesn't exist, but this function should only be called
         if this array exists in NSUserDefaults */
-        let names = NSUserDefaults.standardUserDefaults().objectForKey("NewUserNames") as! [String]
-        
-        // Adding last element in the array, avoids duplicates
-        UserComboBox.addItemWithObjectValue(names.last!)
+        UserComboBox.addItemWithObjectValue((names as! [String]).last!)
     }
     
     // Function that runs when the user selects something in the NSComboBox
@@ -56,26 +84,46 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         // Save to NSUserDefaults under a key of currentUser
         // This will be the identity of the current user that the program will use
-        NSUserDefaults.standardUserDefaults().setObject(savedString, forKey: "currentUser")
+        //NSUserDefaults.standardUserDefaults().setObject(savedString, forKey: "currentUser")
         
         // for debugging
         print("\(savedString) saved to NSUserDefaults")
         
     }
+    // This will tell the first window to close after the main window has opened
+    // Listening for notification name "FirstWindowCloseNotification"
+    func notInFocusAnymore(notification: NSNotification) {
+        self.window?.close()
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
         
+        // debug code
+        //NSUserDefaults.standardUserDefaults().removeObjectForKey("NewUserNames")
+        
         // Disabling the continue button initially as no user would be selected
         ContinueButton.enabled = false
         
-        // Setting up notification listeners to populate the dropdown box of users and to check if the user selected something in the NSComboBox
+        /* Setting up notification listeners to populate the dropdown box of users and to 
+        check if the user selected something in the NSComboBox */
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "populateNSComboBox:", name: "NameDataSavedNotification", object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "comboBoxSelectionDidChange:", name: "NSComboBoxSelectionDidChangeNotification", object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "notInFocusAnymore:", name: "FirstWindowCloseNotification", object: nil)
+        
+        // Initial population of NSComboBox
+        populateNSComboBoxWhenAppLaunches()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+        
+        /* Removing listeners to all notifications created by this app so that other apps can't
+        use them (for stability reasons, less potential crashing in other apps) */
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "NameDataSavedNotification", object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "NSComboBoxSelectionDidChangeNotification", object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "FirstWindowCloseNotification", object: nil)
     }
 
 
---
+++ Project/Weight Tracker/Weight Tracker/MainWindowController.swift
@@ -0,0 +1,72 @@
+//
+//  MainWindowController.swift
+//  Weight Tracker
+//
+//  Created by Alex Potter on 27/08/2015.
+//  Copyright © 2015 Alex Potter. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+    // Connecting IB objects to code
+    @IBOutlet weak var HelloLabel: NSTextField!
+    @IBOutlet weak var LatestWeightLabel: NSTextField!
+    
+    // Loads the user's information and greeter (the two sentences at the top of the window)
+    func setupUser() {
+        let profileName = NSUserDefaults.standardUserDefaults().objectForKey("currentUser") as! String
+        let profileInfo = NSUserDefaults.standardUserDefaults().objectForKey("profileInfo\(profileName)")
+        
+        // Customises the greeter
+        HelloLabel.stringValue = "Hi, \(profileName)"
+        
+        // Displays tracked weight
+        if profileInfo == nil { // shouldn't be nil
+            LatestWeightLabel.stringValue = "No weight goal set yet..."
+        } else {
+            // Get values from dictionary
+            let profileInfoDictionary = profileInfo as! NSMutableDictionary
+            let weightUnitValue = profileInfoDictionary.valueForKey("weightUnit") as! String
+            let weightValueArray = profileInfoDictionary.valueForKey("latestPredictedWeightLoss") as! NSArray
+            let weightGainOrLoss = profileInfoDictionary.valueForKey("latestPredictedGain/Loss") as! Int
+            var LatestWeightLabelValueSet: Bool = false
+            var LatestWeightLabelString: String = ""
+    
+            switch weightUnitValue {
+            case "kg":
+                LatestWeightLabelString = "You're on track for \((weightValueArray as! [String]).last)kg weight "
+            case "lbs":
+                LatestWeightLabelString = "You're on track for \((weightValueArray as! [String]).last)lbs weight "
+            case "stlbs":
+                var stlbsArray: [String] = ((weightValueArray as! [String]).last?.componentsSeparatedByString(";"))!
+                LatestWeightLabelString = "You're on track for \(stlbsArray[0])st \(stlbsArray[1])lbs weight "
+            default:
+                LatestWeightLabel.stringValue = "You're on track for 0kg weight loss"
+                LatestWeightLabelValueSet = true
+            }
+            
+            if LatestWeightLabelValueSet == false {
+                if weightGainOrLoss == 0 { // 0 = loss, anything else = gain
+                    HelloLabel.stringValue = LatestWeightLabelString + "loss"
+                } else {
+                    HelloLabel.stringValue = LatestWeightLabelString + "gain"
+                }
+            }
+        }
+        
+    }
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        
+        // Posts notification to shutdown the other window, as we don't need it anymore
+        NSNotificationCenter.defaultCenter().postNotificationName("FirstWindowCloseNotification", object: nil)
+        
+        setupUser()
+    }
+    
+}
