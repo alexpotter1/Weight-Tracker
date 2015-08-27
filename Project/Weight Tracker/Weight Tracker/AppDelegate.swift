690277941c7d16191e88d0e6fd9afb6292924600
Alex Potter <alexanderpotter2010@gmail.com>
2015-08-27 17:22:15 +0100

    Set up the User Interface and UI progression for the New User pane

--- Project/Weight Tracker/Weight Tracker/AppDelegate.swift
+++ Project/Weight Tracker/Weight Tracker/AppDelegate.swift
@@ -11,9 +11,25 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    // Connecting Interface Builder objects to the code so that they can be used
+    // For MainMenu.xib:
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var ContinueButton: NSButton!
+    @IBOutlet weak var UserComboBox: NSComboBox!
+    
+    /* Keeps a reference to the window controller that we're gonna switch to when
+    we click the New User button, so that it doesn't get deallocated (otherwise, errors) */
+    let newUserVC = NewUserWindowController(windowNibName: "NewUserWindow")
 
+    @IBAction func NewUserButtonClicked(sender: NSButton) {
+        /* Begins the modal sheet animation when the New User button is clicked;
+        this presents the new window on top of the previous window, sliding down
+        into focus */
+        self.window.beginSheet(newUserVC.window!, completionHandler: nil)
+    }
+    
+    @IBAction func ContinueButtonClicked(sender: NSButton) {
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
