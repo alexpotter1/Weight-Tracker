f8dfcf2c7f4bf96910bb06e33fd3d88608c74ae2
Alex Potter <alexanderpotter2010@gmail.com>
2015-09-13 12:47:50 +0100

    Added UI Tests to application
    Initial stages of UI can now be tested

--- Project/Weight Tracker/Weight TrackerUITests/Weight_TrackerUITests.swift
+++ Project/Weight Tracker/Weight TrackerUITests/Weight_TrackerUITests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import Cocoa
 
 class Weight_TrackerUITests: XCTestCase {
         
@@ -25,12 +26,43 @@ class Weight_TrackerUITests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        NSUserDefaults.standardUserDefaults().removeObjectForKey("NewUserNames")
         super.tearDown()
     }
     
-    func testExample() {
+    func testUICreateUser() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        // Code created by Xcode's UI Test Record Feature
+        let weightTrackerWindow = XCUIApplication().windows["Weight Tracker"]
+        weightTrackerWindow.buttons["Create a new user"].click()
+        
+        let sheetsQuery = weightTrackerWindow.sheets
+        sheetsQuery.textFields["Type the new user's name here..."].typeText("Test123")
+        weightTrackerWindow.click()
+        sheetsQuery.buttons["Done"].click()
+        weightTrackerWindow.comboBoxes["Select a user..."].childrenMatchingType(.Button).element.click()
+        weightTrackerWindow.scrollViews.otherElements.childrenMatchingType(.TextField).elementBoundByIndex(0).click()
+        weightTrackerWindow.buttons["Continue"].click()
+        
+    }
+    
+    func testUISetUpUserSettings() {
+        // Code created by Xcode's UI Test Record feature
+        let weightTrackerWindow = XCUIApplication().windows["Weight Tracker"]
+        weightTrackerWindow.comboBoxes["Select a user..."].childrenMatchingType(.Button).element.click()
+        weightTrackerWindow.scrollViews.otherElements.childrenMatchingType(.TextField).element.click()
+        weightTrackerWindow.buttons["Continue"].click()
+        weightTrackerWindow.buttons["Settings..."].click()
+        
+        let popoversQuery = weightTrackerWindow.popovers
+        popoversQuery.childrenMatchingType(.PopUpButton).element.click()
+        weightTrackerWindow.popovers.menuItems["Kilograms (kg)"].click()
+        popoversQuery.buttons["Done"].click()
+        weightTrackerWindow.buttons[XCUIIdentifierCloseWindow].click()
+        
+        
     }
     
 }
