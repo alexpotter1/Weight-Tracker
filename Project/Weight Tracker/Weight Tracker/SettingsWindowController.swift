010221e993bbb322584929d1e6871702d658dd77
Alex Potter <alexanderpotter2010@gmail.com>
2015-09-19 18:39:59 +0100

    SettingsWindowController: Added switch statement
    Shortens the data stored about the weight unit in NSUserDefaults

--- Project/Weight Tracker/Weight Tracker/SettingsWindowController.swift
+++ Project/Weight Tracker/Weight Tracker/SettingsWindowController.swift
@@ -18,15 +18,28 @@ class SettingsWindowController: NSWindowController {
     
     func weightUnitSelectionDidChange(notification: NSNotification) {
         let selectedItem = WeightUnitBox.titleOfSelectedItem
+        // selectedItem is of type String?, so checking if nil first
         if selectedItem != nil {
-            print(selectedItem!)
-            NSUserDefaults.standardUserDefaults().setObject(selectedItem!, forKey: "SettingsWeightUnitItem")
-            NSUserDefaults.standardUserDefaults().synchronize()
+            print(selectedItem!) // debug
+            // Saving shortened value to NSUserDefaults
+            switch selectedItem! {
+            case "Kilograms (kg)":
+                NSUserDefaults.standardUserDefaults().setObject("kg", forKey: "SettingsWeightUnitItem")
+                NSUserDefaults.standardUserDefaults().synchronize()
+            case "Pounds (lbs)":
+                NSUserDefaults.standardUserDefaults().setObject("lbs", forKey: "SettingsWeightUnitItem")
+                NSUserDefaults.standardUserDefaults().synchronize()
+            case "Stone & Pounds (st lbs)":
+                NSUserDefaults.standardUserDefaults().setObject("st lbs", forKey: "SettingsWeightUnitItem")
+                NSUserDefaults.standardUserDefaults().synchronize()
+            default: break
+            }
         }
     }
 
     override func windowDidLoad() {
         super.windowDidLoad()
+        // Launching function when user clicks item in selection box
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "weightUnitSelectionDidChange:", name: NSMenuDidSendActionNotification, object: self.WeightUnitBox.menu)
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
