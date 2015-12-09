b187062ba27e1f857ea7e78206a2ca09a0ad96ed
Alex Potter <alexanderpotter2010@gmail.com>
2015-12-09 11:02:10 +0000

    CorePlot: Add Swift Bridging Header
    Added bridging header to allow for communication between the Swift and Objective-C parts of the project.

---
+++ Project/Weight Tracker/Weight Tracker/Weight_Tracker_Bridging_Header.h
@@ -0,0 +1,9 @@
+//
+//  Weight_Tracker_Bridging_Header.h
+//  Weight Tracker
+//
+//  Created by Alex Potter on 09/12/2015.
+//  Copyright © 2015 Alex Potter. All rights reserved.
+//
+
+#import <CorePlot/CorePlot.h>
